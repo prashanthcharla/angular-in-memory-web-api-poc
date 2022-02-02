48312072da4a719cfb5dc75ffde9b6f4f14b4e85
Prashanth Charla <prashanth.charla@boeing.com>
2022-02-02 23:36:50 +0530

    Overrided the getId function to customise the id generated

--- college-demo/src/app/services/data/in-memory-data.service.ts
+++ college-demo/src/app/services/data/in-memory-data.service.ts
@@ -5,6 +5,10 @@ import { COLLEGE_DATA } from 'src/app/common/helpers/college.data';
 import { LECTURER_DATA } from 'src/app/common/helpers/lecturer.data';
 import { STREAM_DATA } from 'src/app/common/helpers/stream.data';
 import { STUDENT_DATA } from 'src/app/common/helpers/student.data';
+import { College } from 'src/app/models/college';
+import { Lecturer } from 'src/app/models/lecturer';
+import { Stream } from 'src/app/models/stream';
+import { Student } from 'src/app/models/student';
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +24,9 @@ export class InMemoryDataService implements InMemoryDbService {
       lecturer: LECTURER_DATA, 
       college: COLLEGE_DATA 
     };
+  }
+
+  genId<T extends College | Student | Stream | Lecturer>(data: T[]): number {
+    return data.length > 0 ? Math.max(...data.map(t => t.id)) + 1 : 1;
   }
 }
