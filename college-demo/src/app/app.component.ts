0a719e7b0edda1cf305961d7c150086bb589b258
Prashanth Charla <prashanth.charla@boeing.com>
2022-02-02 23:41:43 +0530

    Made get all API's to be called on application load

--- college-demo/src/app/app.component.ts
+++ college-demo/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Stream } from './models/stream';
+import { CollegeService } from './services/functional/college.service';
+import { LecturerService } from './services/functional/lecturer.service';
+import { StreamService } from './services/functional/stream.service';
+import { StudentService } from './services/functional/student.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'college-demo';
+  stream: Stream;
+
+  constructor(private streamService: StreamService,
+    private lecturerServive: LecturerService,
+    private studentService: StudentService,
+    private collegeService: CollegeService) {}
+
+  ngOnInit(): void {
+    this.streamService.getAllStreamsDetails();
+    this.lecturerServive.getAllLecturerDetails();
+    this.studentService.getAllStudentsDetails();
+    this.collegeService.getAllCollegesDetails();
+  }
 }
