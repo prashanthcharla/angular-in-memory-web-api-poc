b2b14b84c46338e63b741fc508738086fde2c5b3
Prashanth Charla <prashanth.charla@boeing.com>
2022-02-06 22:43:21 +0530

    Create separate file to store constants which are used across the application

---
+++ college-demo/src/app/common/Constants.ts
@@ -0,0 +1,11 @@
+export const COLLEGE_LOADING_MESSAGE: string = "Getting collges data. Please wait...";
+
+export const ERROR_MESSAGE: string = "Looks like there is some issue. Please try again.";
+
+export const STUDENT_CREATE_FORM_HEADER: string = "Student Registration";
+
+export const STUDENT_UPDATE_FORM_HEADER: string = "Update Student Details";
+
+export const LECTURER_CREATE_FORM_HEADER: string = "Lecturer Registration";
+
+export const LECTURER_UPDATE_FORM_HEADER: string = "Update Lecturer Details";
