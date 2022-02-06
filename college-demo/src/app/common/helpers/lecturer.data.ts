89955bec4f70249615895d3cd6283e0ec76ab528
Prashanth Charla <prashanth.charla@boeing.com>
2022-02-06 22:36:15 +0530

    Added college id's to default lecturer data

--- college-demo/src/app/common/helpers/lecturer.data.ts
+++ college-demo/src/app/common/helpers/lecturer.data.ts
@@ -5,36 +5,42 @@ export const LECTURER_DATA: Array<Lecturer> = [
         id: 1,
         streamId: 1,
         firstName: "Pavan Kumar",
-        lastName: "Erraboina"
+        lastName: "Erraboina",
+        collegeId: 1
     },
     {
         id: 2,
         streamId: 2,
         firstName: "Akhilesh",
-        lastName: "Yadav"
+        lastName: "Yadav",
+        collegeId: 1
     },
     {
         id: 3,
         streamId: 3,
         firstName: "Satish",
-        lastName: "Reddy"
+        lastName: "Reddy",
+        collegeId: 1
     },
     {
         id: 4,
         streamId: 4,
         firstName: "Ashok",
-        lastName: "Reddy"
+        lastName: "Reddy",
+        collegeId: 1
     },
     {
         id: 5,
         streamId: 5,
         firstName: "Madhu Priya",
-        lastName: "Putti"
+        lastName: "Putti",
+        collegeId: 1
     },
     {
         id: 6,
         streamId: 6,
         firstName: "Sowjanya",
-        lastName: "Reddy"
+        lastName: "Reddy",
+        collegeId: 1
     },
 ];
